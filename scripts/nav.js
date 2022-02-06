283b5f5ce6d54f253fd874502bec730f6c718bae
ArjunGTX <arjunatpavilion@gmail.com>
2022-02-06 21:20:11 +0530

    Feat: added dynamic navlink state
    navLinks now chnage their state based on which section is visible on the screen

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,10 +1,12 @@
 const closeIcon = document.querySelector(".menu-close");
 const openIcon = document.querySelector(".menu-open");
 const nav = document.querySelector("nav");
-const navLinks = document.querySelectorAll(".nav-link");
+const sectionList = document.querySelectorAll(".section-head");
 
+//toggling Navbar
 openIcon.addEventListener("click", () => nav.classList.add("nav-open"));
 closeIcon.addEventListener("click", () => nav.classList.remove("nav-open"));
+
 //code to close nav bar when clicked outside
 document.addEventListener(
     "click",
@@ -15,12 +17,22 @@ document.addEventListener(
     },
     true
 );
-//code to maintain active link
-navLinks.forEach((navLink) =>
-    navLink.addEventListener("click", (e) => {
-        console.log(e.target);
-        let activeLink = document.querySelector(".nav-link-active");
-        activeLink && activeLink.classList.remove("nav-link-active");
-        e.target.classList.add("nav-link-active");
-    })
-);
+
+//code to maintain active state of navLinks
+const observationHandler = (entries) => {
+    entries.forEach((entry) => {
+        //selecting navLink whose href section is visible on viewport
+        const link =
+            entry.isIntersecting === true &&
+            document.querySelector(`[href="#${entry.target.id}"]`);
+        // selecting previously active navLink
+        const activeLink = document.querySelector(".nav-link-active");
+        //setting active state to navLink based on whether its corresponding section is visible on the screen
+        if (entry.isIntersecting === true) {
+            activeLink && activeLink.classList.remove("nav-link-active");
+            link.classList.add("nav-link-active");
+        } else link && link.classList.remove("nav-link-active");
+    });
+};
+const observer = new IntersectionObserver(observationHandler);
+sectionList.forEach((section) => observer.observe(section));
